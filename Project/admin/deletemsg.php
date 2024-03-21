9f5596f37dae44a0dbc97bfb89522457b3eb0af3
Om Gadekar <74897702+omgadekar@users.noreply.github.com>
2024-03-21 10:07:41 +0530

    Delete Project/admin directory

--- Project/admin/deletemsg.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'head.php';
-mysqli_query($con,"delete from contactus where msg_id=".$_GET["msg_id"]);
-header("location:message.php");
-?>
--- Project/admin/remove.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'head.php';
-mysqli_query($con,"delete from tblproduct where pid=".$_GET["pid"]);
-header("location:removeproduct.php");
-?>
